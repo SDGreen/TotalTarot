513dfa37ff0df6ad1791353270742315a65cf426
SDGreen <sad_green@hotmail.co.uk>
2020-06-21 23:56:37 +0000

    darkMode() working

--- assets/scripts/dark-mode.js
+++ assets/scripts/dark-mode.js
@@ -5,18 +5,18 @@ function darkMode(mode){
     
      if(mode === "light-mode"){
 
-         let lightElements = document.getElementsByClassName("light-mode");
+         let lightElements = [].slice.call(document.getElementsByClassName("light-mode"));
          console.log(lightElements);
 
         for (element of lightElements){
             element.classList.add("dark-mode");
             element.classList.remove("light-mode");
         };
-            modeButton.outerHTML = `<div id="mode-button" onclick="darkMode('dark-mode')" class="nav-item light-mode d-none d-sm-inline-block"></div>`;
+            modeButton.outerHTML = `<div id="mode-button" onclick="darkMode('dark-mode')" class="nav-item dark-mode d-none d-sm-inline-block"></div>`;
             navModeButton.outerHTML = `<li id="mode-collapse-button" onclick="darkMode('dark-mode')" class="nav-item d-sm-none"><a class="nav-link">Light Mode</a></li>`;
     } else {
 
-        let darkElements = document.getElementsByClassName("dark-mode");
+        let darkElements = [].slice.call(document.getElementsByClassName("dark-mode"));
 
         for (element of darkElements){
             element.classList.add("light-mode");
