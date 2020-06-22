3d8cdbeef058cd83e91917cf1e4f49c8ebfc4ecf
SDGreen <sad_green@hotmail.co.uk>
2020-06-22 21:27:53 +0000

    footer added with funtioning dark mode

--- assets/scripts/dark-mode.js
+++ assets/scripts/dark-mode.js
@@ -2,11 +2,12 @@ function darkMode(mode){
 
     let modeButton = document.getElementById("mode-button");
     let navModeButton = document.getElementById("mode-collapse-button");
+    let footerModeButton = document.getElementById("footer-mode-button");
+    let currentModeLabel = document.getElementById("current-mode");
     
      if(mode === "light-mode"){
 
          let lightElements = [].slice.call(document.getElementsByClassName("light-mode"));
-         console.log(lightElements);
 
         for (element of lightElements){
             element.classList.add("dark-mode");
@@ -14,6 +15,8 @@ function darkMode(mode){
         };
             modeButton.outerHTML = `<div id="mode-button" onclick="darkMode('dark-mode')" class="nav-item dark-mode d-none d-sm-inline-block"></div>`;
             navModeButton.outerHTML = `<li id="mode-collapse-button" onclick="darkMode('dark-mode')" class="nav-item d-sm-none"><a class="nav-link">Light Mode</a></li>`;
+            footerModeButton.outerHTML = `<div id="footer-mode-button" onclick="darkMode('dark-mode')" class="dark-mode">`
+            currentModeLabel.innerText = "dark mode"
     } else {
 
         let darkElements = [].slice.call(document.getElementsByClassName("dark-mode"));
@@ -25,5 +28,7 @@ function darkMode(mode){
         
         modeButton.outerHTML = `<div id="mode-button" onclick="darkMode('light-mode')" class="nav-item light-mode d-none d-sm-inline-block"></div>`;
         navModeButton.outerHTML = `<li id="mode-collapse-button" onclick="darkMode('light-mode')" class="nav-item d-sm-none"><a class="nav-link">Dark Mode</a></li>`;
+        footerModeButton.outerHTML = `<div id="footer-mode-button" onclick="darkMode('light-mode')" class="light-mode">`
+        currentModeLabel.innerText = "light mode"
     }
 }
