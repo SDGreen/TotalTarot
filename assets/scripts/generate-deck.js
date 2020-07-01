160e321c159db6610f12a7aaca52e0759d220d8d
SDGreen <sad_green@hotmail.co.uk>
2020-07-01 13:22:11 +0000

    deck generating

--- assets/scripts/generate-deck.js
+++ assets/scripts/generate-deck.js
@@ -4,8 +4,7 @@ $(document).ready(function(){
         $.getJSON(`https://tarot.howlcode.com/api/v1/cards`)
     ).then(
         function(response) {
-            var fullDeckInfo = response;
-            generateDeck();
+            generateDeck(response);
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
@@ -18,22 +17,27 @@ $(document).ready(function(){
             }
         });
 
-    function generateDeck(){
+    function generateDeck(deck){
+
+        var deckInfo = deck;
 
         function getCardName(id){
-            return fullDeckInfo + "." + id + ".name";
+            let rawCardName = deckInfo[id].name;
+            cardName = rawCardName.replace(/-/g, " ");
+            return cardName;
         }
 
-        for (i = 0; i < 78; i++) {
+
+        for (i = 0; i < 79; i++) {
             let cardId = i+1;
-            let cardName = getCardName(cardId)
-            let card = `<div class="card col" id="${cardId}">
+            let cardName = getCardName(i)
+            let card = `<div class="card col-auto" id="${cardId}">
                             <div class="row">
-                                <div class="col-12 card-image">
-                                    <img src="assets/images/card-back.jpg" alt="Back of card">
+                                <div class="col-12">
+                                    <img class="mx-auto d-block" src="assets/images/card-back.jpg" alt="Back of card">
                                 </div>
                                 <div class="col-12 card-name">
-                                    <p class="text-center">${cardName}</p>
+                                    <p class="text-center sm-heading">${cardName}</p>
                                 </div>
                             </div>
                         </div>`
