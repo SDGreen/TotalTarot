e25f64f43ec66a480e7b3d75f2a4ca3160fc7cea
SDGreen <sad_green@hotmail.co.uk>
2020-07-01 22:38:55 +0000

    card info loading

--- assets/scripts/generate-deck.js
+++ assets/scripts/generate-deck.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
         function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#deck").html(
-                    `<h2>Whoops looks like the future is shrouded!</h2>`);
+                    `<h2>Whoops! looks like the future cannot be found...</h2>`);
             } else {
                 console.log(errorResponse);
                 $("#deck").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                    console.log(errorResponse.responseJSON)
             }
-        });
+    });
 
     function generateDeck(deck){
 
@@ -31,17 +32,23 @@ $(document).ready(function(){
         for (i = 0; i < 79; i++) {
             let cardId = i+1;
             let cardName = getCardName(i)
+            let cardInfo = deckInfo.i
             let card = `<div class="card col-auto" id="${cardId}">
                             <div class="row">
-                                <div class="col-12">
-                                    <img class="mx-auto d-block" src="assets/images/card-back.jpg" alt="Back of card" onclick="showCardInfo(${cardId})">
+                                <div class="col-12 text-center">
+                                    <div class="card-image d-inline-block" onclick="showCardInfo(${cardId})"></div>
                                 </div>
                                 <div class="col-12 card-text">
                                     <p class="text-center sm-heading card-name" onclick="showCardInfo(${cardId})">${cardName}</p>
+                                    <div class="card-info rounded">
+                                    </div>
                                 </div>
                             </div>
                         </div>`
             $("#deck").append(card);
+            $("card-info").slideUp();
         };
     }
+
+
 });
--- assets/scripts/show-card-details.js
+++ assets/scripts/show-card-details.js
@@ -1,3 +1,68 @@
 function showCardInfo(id){
-    console.log("this is card number " + id);
+    let cardId = id;
+    let mode = localStorage.mode
+    $("#deck").find(".card-name").fadeIn();
+    $("#deck").find(".card-info").slideUp().removeClass("shown light-mode dark-mode");
+    $(`#${cardId} .card-name`).fadeOut();
+    $(`#${cardId} .card-info`).addClass(`shown ${mode}`).slideDown()
+
+     $.when(
+        $.getJSON("https://tarot.howlcode.com/api/v1/cards/" + cardId)
+    ).then(
+        function(response) {
+            renderCardData(response, displayData);
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#deck").html(
+                    `<h2>Whoops! looks like the future cannot be found...</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#deck").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+                    console.log(errorResponse.responseJSON)
+            }
+    });
+
+    function displayData(cardInfo){
+        $(`#${cardId} div.card-info`).html(cardInfo);
+    }
+
+    function renderCardData(data, callback){
+        console.log(data);
+        let cardData = `<div class="row">
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h3 class="sm-heading text-center">Upright Meaning:</h3>
+                                    </div>
+                                    <div class="col-12">
+                                        <p>${data.upright}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h3 class="sm-heading text-center">Inverted Meaning:</h3>
+                                    </div>
+                                    <div class="col-12">
+                                        <p>${data.reversed}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h3 class="sm-heading text-center">Summary:</h3>
+                                    </div>
+                                    <div class="col-12">
+                                        <p>${data.summary}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+        callback(cardData);
+    }
+
 };
