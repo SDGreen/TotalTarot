71c9e567f578d2693f618e646ab6b6a19d2cf59d
SDGreen <sad_green@hotmail.co.uk>
2020-07-02 16:29:27 +0000

    card selector updated

--- assets/scripts/find-card.js
+++ assets/scripts/find-card.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $("#cardBtn").click(function(){
         let elementId = $("select").val()
+        showCardInfo(elementId);
         let element = document.getElementById(elementId);
         element.scrollIntoView({behavior: "smooth", block: "start"});
-        showCardInfo(elementId);
     });
 });
