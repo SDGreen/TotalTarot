4bb73c5898aec426e7bcf6ff39a55b91dd502fde
SDGreen <sad_green@hotmail.co.uk>
2020-07-02 17:14:17 +0000

    back to top button added on deck page

--- assets/scripts/find-card.js
+++ assets/scripts/find-card.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
         let elementId = $("select").val()
         showCardInfo(elementId);
         let element = document.getElementById(elementId);
-        element.scrollIntoView({behavior: "smooth", block: "start"});
+        element.scrollIntoView({behavior: "smooth", block: "end"});
     });
+
+    window.onscroll = function(){scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+            $("#back-to-top").fadeIn().css("display", "block")
+        } else {
+            $("#back-to-top").fadeOut();
+    }
+
+    $("#back-to-top").click(function(){
+        $(window).scrollTop(0);
+    })
+}
 });
