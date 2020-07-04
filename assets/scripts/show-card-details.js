c7606e34df67bbaa03c38c97acb84c3b035e2a46
SDGreen <sad_green@hotmail.co.uk>
2020-07-04 11:48:26 +0000

    card flip now only works if card is not visible to begin with

--- assets/scripts/show-card-details.js
+++ assets/scripts/show-card-details.js
@@ -1,80 +1,91 @@
 function showCardInfo(id){
     let cardId = id;
+
+    /* checks the current mode for correct colouring*/
     let mode;
     if (localStorage.mode === "light-mode"){
         mode = "dark-mode";
     } else {
         mode = "light-mode"
     }
-    $("#deck").find(".card-name").fadeIn();
-    $("#deck").find(".flipped").removeClass("flipped");
-    $("#deck").find(".card-info").slideUp().removeClass("shown light-mode dark-mode");
-    $(`#${cardId} .card-name`).fadeOut();
-    $(`#${cardId} .card-info`).addClass(`shown ${mode}`).slideDown()
 
-     $.when(
-        $.getJSON("https://tarot.howlcode.com/api/v1/cards/" + cardId)
-    ).then(
-        function(response) {
-            renderCardData(response, displayData, cardFlip);
-        },
-        function(errorResponse) {
-            if (errorResponse.status === 404) {
-                $("#deck").html(
-                    `<h2 class="sm-heading">Whoops! Looks like the future cannot be found...</h2>`);
-            } else {
-                console.log(errorResponse);
-                $("#deck").html(
-                    `<h2 class="content-text">Error: ${errorResponse.responseJSON.message}</h2>`);
-                    console.log(errorResponse.responseJSON)
-            }
-    });
+    /* checks the card hasn't already been flipped */   
+    let currentCardMode = $(`#${cardId} .card-name`).css("display")
+    if (currentCardMode === "none"){
+        return;
+    } else {
 
-    function displayData(cardInfo){
-        $(`#${cardId} div.card-info`).html(cardInfo);
-    }
+        /* runs the code to flip the card and get card data */
+        $("#deck").find(".card-name").fadeIn();
+        $("#deck").find(".flipped").removeClass("flipped");
+        $("#deck").find(".card-info").slideUp().removeClass("shown light-mode dark-mode");
+        $(`#${cardId} .card-name`).fadeOut();
+        $(`#${cardId} .card-info`).addClass(`shown ${mode}`).slideDown()
 
-    function cardFlip(cardData){
-        let backgroundImage = {"background": `url(${cardData.image}) no-repeat center/contain`}
-        $(`#${cardId} .card-back`).css(backgroundImage);
-        $(`#${cardId} .card-inner`).addClass("flipped");
-    }
+        $.when(
+            $.getJSON("https://tarot.howlcode.com/api/v1/cards/" + cardId)
+        ).then(
+            function(response) {
+                renderCardData(response, displayData, cardFlip);
+            },
+            function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $("#deck").html(
+                        `<h2 class="sm-heading">Whoops! Looks like the future cannot be found...</h2>`);
+                } else {
+                    console.log(errorResponse);
+                    $("#deck").html(
+                        `<h2 class="content-text">Error: ${errorResponse.responseJSON.message}</h2>`);
+                        console.log(errorResponse.responseJSON)
+                }
+        });
 
-    function renderCardData(data, callbackOne, callbackTwo){
-        let cardData = `<div class="row">
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <h3 class="sm-heading text-center">Upright Meaning:</h3>
-                                    </div>
-                                    <div class="col-12">
-                                        <p class="slim-content-text">${data.upright}</p>
+        function displayData(cardInfo){
+            $(`#${cardId} div.card-info`).html(cardInfo);
+        }
+
+        function cardFlip(cardData){
+            let backgroundImage = {"background": `url(${cardData.image}) no-repeat center/contain`}
+            $(`#${cardId} .card-back`).css(backgroundImage);
+            $(`#${cardId} .card-inner`).addClass("flipped");
+        }
+
+        function renderCardData(data, callbackOne, callbackTwo){
+            let cardData = `<div class="row">
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <h3 class="sm-heading text-center">Upright Meaning:</h3>
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="slim-content-text">${data.upright}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <h3 class="sm-heading text-center">Inverted Meaning:</h3>
-                                    </div>
-                                    <div class="col-12">
-                                        <p class="slim-content-text">${data.reversed}</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <h3 class="sm-heading text-center">Summary:</h3>
-                                    </div>
-                                    <div class="col-12">
-                                        <p class="slim-content-text">${data.summary}</p>
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <h3 class="sm-heading text-center">Inverted Meaning:</h3>
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="slim-content-text">${data.reversed}</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>`
-        callbackTwo(data);
-        callbackOne(cardData);
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <h3 class="sm-heading text-center">Summary:</h3>
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="slim-content-text">${data.summary}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>`
+            callbackTwo(data);
+            callbackOne(cardData);
+        }
     }
 
 };
